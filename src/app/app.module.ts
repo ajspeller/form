bded236c1c651ecce4cd733a1c873ad1fe65e418
AJ Speller <ajspeller@gmail.com>
2020-01-22 22:56:23 -0500

    Added styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardFormComponent,
-    InputComponent
-  ],
+  declarations: [AppComponent, CardFormComponent, InputComponent],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgxMaskModule.forRoot(options)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DateFormControl } from '../date-form-control';
 
 @Component({
   selector: 'app-card-form',
@@ -13,10 +14,21 @@ export class CardFormComponent implements OnInit {
       Validators.maxLength(5),
       Validators.minLength(3)
       // Validators.pattern(/\s/)
+    ]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(16),
+      Validators.minLength(16)
+    ]),
+    expDate: new DateFormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(3),
+      Validators.minLength(3)
     ])
-    // cardNumber: new FormControl(''),
-    // expDate: new FormControl('')
-    // securityCode: new FormControl('')
   });
 
   constructor() {
@@ -24,4 +36,12 @@ export class CardFormComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  onSubmit() {
+    console.log('form submitted');
+  }
+
+  onResetClick() {
+    this.cardForm.reset();
+  }
 }
